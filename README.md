a46c27f36463e69b2442e50dcb51d839b6f3adf8
ljp <460222822@qq.com>
2021-03-01 00:06:52 +0800

    添加组件构建模式

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # autoBackground
 
-> 该项目为自适应后台管理系统，目地是简化开发过程中对数据库增删改查测试流程，只需绑定后台增删改查接口即可实现增删改查测试。该项目以vue为前端框架，二次封装elmentui，实现了autoTable、autoForm、autoBackground组件。
+> 该项目为自适应后台管理系统，目地是简化开发过程中对数据库增删改查测试流程，只需绑定后台增删改查接口即可实现增删改查测试。该项目以vue为前端框架，二次封装elmentui，实现了autoTable、autoForm、autoBackground组件。该项目为自适应后台管理系统，目地是简化开发过程中对数据库增删改查测试流程，只需绑定后台增删改查接口即可实现增删改查测试。
+
+## 目录结构
+
+``` lua
+├── build --项目构建(webpack)相关代码
+├── config  --配置目录，包括端口号等
+├── node_modules    --npm 加载的项目依赖模块
+├── examples    --用于展示组件的demo，把程序主入口配置在这里了
+├── packages    --组件代码
+├── static  --静态资源目录，如图片、字体等
+├── test    --测试相关
+├── index.html  --首页入口文件，你可以添加一些 meta 信息或统计代码啥的
+├── package.json    --项目配置文件
+├── README.md   --项目的说明文档，markdown 格式
+```
 ## Build Setup
 
 ``` bash
---
+++ build/webpack.lib.conf.js
@@ -0,0 +1,88 @@
+var path = require("path");
+var webpack = require('webpack')
+module.exports = {
+  entry: './packages/auto-bg-management/index.js',
+  output: {
+    path: path.resolve(__dirname, '../lib'),
+    publicPath: '../lib/',
+    filename: 'auto-bg-management.js',
+    library: 'auto-bg-management', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd',// 指定输出格式
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/lib/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints:false
+  }
+}

8e1b5b7fc2168e36ea3d7c7532749479cdc35416
ljp <460222822@qq.com>
2021-06-03 10:44:42 +0800

    继续修复

--- packages/index.js
+++ packages/index.js
@@ -8,21 +8,6 @@ import autoCardHome from './auto-home'
 import autoSimplePage from './auto-simple-page'
 const packages = { autoTest, autoTable, autoForm, autoBM, autoCarousel, autoCardList, autoCardHome, autoSimplePage};
 console.log(packages);
-// function init(Vue) {
-//     if (Vue) {
-//         for (let key in packages) {
-//             Vue.use(packages[key])
-//         }
-//     }
-// }
-
-// if (typeof window !== 'undefined' && window.Vue) {
-//     init(window.Vue);
-// }
-
-// packages.install = function (vue) {//vue-cli中使用该组件
-//     init(vue);
-// }
 export default packages
 
 
